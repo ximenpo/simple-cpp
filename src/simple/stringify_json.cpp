f68510abd7f2a794c57ec52473053ff9ac3502ee
ximenpo <ximenpo@jiandan.ren>
2015-03-20 00:32:20 +0800

    增加 stringify_json 的实现，可以生成json流或从json流中构造 stringify_data 数据。

---
+++ src/simple/stringify_json.cpp
@@ -0,0 +1,276 @@
+
+#include	<cassert>
+#include	<deque>
+
+#include	"simple/string.h"
+#include	"simple/stringify.h"
+#include	"simple/stringify_json.h"
+
+#include	"simple/third/json.h"
+
+struct	stringify_data_json_visitor	:	public stringify_data_visitor {
+    virtual		bool	visit_begin() {
+        seqs_.clear();
+        seqs_.push_back(0);
+        return	os_.good();
+    }
+    virtual		bool	visit_value(const std::string& value, const std::string* name) {
+        write_comma_();
+        if(0 != name) {
+            os_	<<	'"'	<<	(*name)	<<	"\":"	;
+        }
+        if(string_isnumber(value)) {
+            os_<<	value;
+        } else {
+            os_<<	'"'	<<	string_json_escape(value)	<<	'"';
+        }
+        return	os_.good();
+    }
+    virtual		bool	visit_array_begin(const std::string* name) {
+        write_comma_();
+        if(0 != name) {
+            os_	<<	'"'	<<	(*name)	<<	"\":"	;
+        }
+        os_	<<	'[';
+        seqs_.push_back(0);
+        return	os_.good();
+    }
+    virtual		bool	visit_array_end() {
+        os_	<<	']';
+        seqs_.pop_back();
+        return	os_.good();
+    }
+    virtual		bool	visit_object_begin(const std::string* name) {
+        write_comma_();
+        if(0 != name) {
+            os_	<<	'"'	<<	(*name)	<<	"\":"	;
+        }
+        os_	<<	'{';
+        seqs_.push_back(0);
+        return	os_.good();
+    }
+    virtual		bool	visit_object_end() {
+        os_	<<	'}';
+        seqs_.pop_back();
+        return	os_.good();
+    }
+    virtual		bool	visit_end() {
+        assert(!seqs_.empty());
+        seqs_.pop_back();
+        assert(seqs_.empty());
+        return	os_.good();
+    }
+
+    stringify_data_json_visitor(std::ostream& os) : os_(os) {
+    }
+
+    void	write_comma_() {
+        assert(!seqs_.empty());
+        if(seqs_.back() > 0) {
+            os_	<<	',';
+        }
+        seqs_.back()++;
+    }
+
+    std::ostream&		os_;
+    std::deque<size_t>	seqs_;
+};
+
+bool	stringify_to_json_stream(stringify_data& data, std::ostream& os) {
+    stringify_data_json_visitor	visitor(os);
+    return	data.accept(visitor);
+}
+
+static	bool	load_from_json_array(stringify_data& data, stringify::node_id node_id, const json::Array& val);
+static	bool	load_from_json_object(stringify_data& data, stringify::node_id node_id, const json::Object& val);
+
+bool	stringify_from_json_stream(stringify_data& data, std::istream& is) {
+    data.clear();
+
+    json::Value	val;
+    {
+        std::string doc;
+        doc.reserve(1024*4);
+        std::getline(is, doc, (char)-1);// -1 means EOF
+
+        val	= json::Deserialize(doc);
+        if(val.GetType() == json::NULLVal) {
+            return	false;
+        }
+    }
+
+    //
+    //	construct a stringify_data from json::Value
+    //
+
+    //	¸ù½Úµã
+    bool	simple_value	= true;
+    std::ostringstream		os;
+    switch(val.GetType()) {
+    case	json::StringVal:
+        os << val.ToString();
+        break;
+    case	json::IntVal:
+        os << val.ToInt();
+        break;
+    case	json::FloatVal:
+        os << val.ToFloat();
+        break;
+    case	json::DoubleVal:
+        os << val.ToDouble();
+        break;
+    case	json::BoolVal:
+        os << val.ToBool();
+        break;
+    case	json::ObjectVal:
+        simple_value	= false;
+        break;
+    case	json::ArrayVal:
+        simple_value	= false;
+        break;
+    }
+
+    if(simple_value) {
+        stringify::node_value*	value	= 0;
+        data.add_root(&value, 0);
+        value->value	= os.str();
+        return	true;
+    }
+
+    stringify::node_container*	container	= 0;
+    stringify::node_id			container_id= 0;
+    switch(val.GetType()) {
+    case	json::ArrayVal:
+        data.add_root(&container, &container_id);
+        container->is_array	= true;
+        return	load_from_json_array(data, container_id, val.ToArray());
+    case	json::ObjectVal:
+        data.add_root(&container, &container_id);
+        container->is_array	= false;
+        return	load_from_json_object(data, container_id, val.ToObject());
+    }
+
+    return	false;
+}
+
+static	bool	load_from_json_array(stringify_data& data, stringify::node_id node_id, const json::Array& val) {
+    std::ostringstream		os;
+    stringify::node_container*	container	= 0;
+    stringify::node_id			container_id= 0;
+
+    json::Array::ValueVector::const_iterator	it		= val.begin();
+    json::Array::ValueVector::const_iterator	it_end	= val.end();
+    for(; it != it_end; ++it) {
+        os.str("");
+        bool	is_value	= true;
+        switch(it->GetType()) {
+        case json::ObjectVal:
+            is_value	= false;
+            if(!data.add_container(node_id, &container, &container_id)) {
+                return	false;
+            }
+            container->is_array	= false;
+            if(!load_from_json_object(data, container_id, it->ToObject())) {
+                return	false;
+            }
+            break;
+        case json::ArrayVal:
+            is_value	= false;
+            if(!data.add_container(node_id, &container, &container_id)) {
+                return	false;
+            }
+            container->is_array	= true;
+            if(!load_from_json_array(data, container_id, it->ToArray())) {
+                return	false;
+            }
+            break;
+        case json::StringVal:
+            os	<<	it->ToString();
+            break;
+        case json::IntVal:
+            os	<<	it->ToInt();
+            break;
+        case json::FloatVal:
+            os	<<	it->ToFloat();
+            break;
+        case json::DoubleVal:
+            os	<<	it->ToDouble();
+            break;
+        case json::BoolVal:
+            os	<<	it->ToBool();
+            break;
+        default:
+            is_value	= false;
+            break;
+        }
+
+        if(is_value) {
+            stringify::node_value*	value	= 0;
+            data.add_value(node_id, &value, 0);
+            value->value	= os.str();
+        }
+    }
+
+    return	true;
+}
+
+static	bool	load_from_json_object(stringify_data& data, stringify::node_id node_id, const json::Object& val) {
+    std::ostringstream		os;
+    stringify::node_container*	container	= 0;
+    stringify::node_id			container_id= 0;
+
+    json::Object::ValueMap::const_iterator	it		= val.begin();
+    json::Object::ValueMap::const_iterator	it_end	= val.end();
+    for(; it != it_end; ++it) {
+        os.str("");
+        bool	is_value	= true;
+        switch(it->second.GetType()) {
+        case json::ObjectVal:
+            is_value	= false;
+            if(!data.add_container(node_id, it->first, &container, &container_id)) {
+                return	false;
+            }
+            container->is_array	= false;
+            if(!load_from_json_object(data, container_id, it->second.ToObject())) {
+                return	false;
+            }
+            break;
+        case json::ArrayVal:
+            is_value	= false;
+            if(!data.add_container(node_id, it->first, &container, &container_id)) {
+                return	false;
+            }
+            container->is_array	= true;
+            if(!load_from_json_array(data, container_id, it->second.ToArray())) {
+                return	false;
+            }
+            break;
+        case json::StringVal:
+            os	<<	it->second.ToString();
+            break;
+        case json::IntVal:
+            os	<<	it->second.ToInt();
+            break;
+        case json::FloatVal:
+            os	<<	it->second.ToFloat();
+            break;
+        case json::DoubleVal:
+            os	<<	it->second.ToDouble();
+            break;
+        case json::BoolVal:
+            os	<<	it->second.ToBool();
+            break;
+        default:
+            is_value	= false;
+            break;
+        }
+
+        if(is_value) {
+            stringify::node_value*	value	= 0;
+            data.add_value(node_id, it->first, &value, 0);
+            value->value	= os.str();
+        }
+    }
+
+    return	true;
+}
---
+++ tests/test_stringify_json.cpp
@@ -0,0 +1,224 @@
+#include <igloo/igloo.h>
+using namespace igloo;
+
+#include "simple/stringify.h"
+#include "simple/stringify_json.h"
+
+Context(stringify_json_context) {
+    stringify_data	data;
+    void	SetUp() {
+        data.clear();
+    }
+
+    Spec(simple_value_to_json_1) {
+        stringify_data_builder(data).begin()
+        .value(int(34))
+        .end();
+
+        std::ostringstream	os;
+        AssertThat(stringify_to_json_stream(data, os),	IsTrue());
+        AssertThat(os.str(),	Equals(
+                       "34"
+                   ));
+    }
+
+    Spec(simple_value_to_json_2) {
+        stringify_data_builder(data).begin()
+        .value("XiMenPo")
+        .end();
+
+        std::ostringstream	os;
+        AssertThat(stringify_to_json_stream(data, os),	IsTrue());
+        AssertThat(os.str(),	Equals(
+                       "\"XiMenPo\""
+                   ));
+    }
+
+    Spec(to_json_usage) {
+        stringify_data_builder(data).begin()
+        .object_begin()
+        .field("age",	int(34))
+        .field("name",	"XiMenPo")
+        .object_begin("object")
+        .field("sex",	true)
+        .field("sname",	std::string("XiMenPo"))
+        .object_end()
+        .array_begin("array")
+        .value(101)
+        .value(102)
+        .value(103)
+        .array_end()
+        .field("another_data",	"another_data")
+        .object_end()
+        .end();
+
+        std::ostringstream	os;
+        AssertThat(stringify_to_json_stream(data, os),	IsTrue());
+        AssertThat(os.str(),	Equals("{\"age\":34,\"name\":\"XiMenPo\",\""
+                                       "object\":{\"sex\":1,\"sname\":\"XiMenPo\"},"
+                                       "\"array\":[101,102,103],\"another_data\":\"another_data\"}"
+                                   ));
+    }
+
+    Spec(to_json_mixed_object_usage) {
+        stringify_data_builder(data).begin()
+        .object_begin()
+        .field("age",	int(34))
+        .field("name",	"XiMenPo")
+        .object_begin("object")
+        .field("sex",	true)
+        .array_begin("array")
+        .value(101)
+        .value(102)
+        .value(103)
+        .array_end()
+        .field("sname",	std::string("XiMenPo"))
+        .object_end()
+        .field("another_data",	"another_data")
+        .object_end()
+        .end();
+
+        std::ostringstream	os;
+        AssertThat(stringify_to_json_stream(data, os),	IsTrue());
+        AssertThat(os.str(),	Equals("{\"age\":34,\"name\":\"XiMenPo\","
+                                       "\"object\":{\"sex\":1,\"array\":[101,102,103],\"sname\":\"XiMenPo\"},"
+                                       "\"another_data\":\"another_data\"}"
+                                   ));
+    }
+
+    Spec(array_of_array_to_xml_usage) {
+        stringify_data_builder(data).begin()
+        .object_begin()
+        .field("age",	int(34))
+        .field("name",	"XiMenPo")
+        .object_begin("object")
+        .field("sex",	true)
+        .field("sname",	std::string("XiMenPo"))
+        .object_end()
+        .array_begin("array")
+        .array_begin()
+        .value(101)
+        .value(102)
+        .value(103)
+        .array_end()
+        .array_begin()
+        .value(101)
+        .value(102)
+        .value(103)
+        .array_end()
+        .array_begin()
+        .value(101)
+        .value(102)
+        .value(103)
+        .array_end()
+        .array_end()
+        .field("another_data",	"another_data")
+        .object_end()
+        .end();
+
+        std::ostringstream	os;
+        AssertThat(stringify_to_json_stream(data, os),	IsTrue());
+        AssertThat(os.str(),	Equals(
+                       "{"
+                       "\"age\":34,"
+                       "\"name\":\"XiMenPo\","
+                       "\"object\":{\"sex\":1,\"sname\":\"XiMenPo\"},"
+                       "\"array\":["
+                       "[101,102,103],"
+                       "[101,102,103],"
+                       "[101,102,103]"
+                       "],"
+                       "\"another_data\":\"another_data\""
+                       "}"
+                   ));
+    }
+
+    Spec(from_stream_usage) {
+        // sorted members.
+        std::string	json	= "{\"age\":34,\"another_data\":\"another_data\","
+                              "\"name\":\"XiMenPo\","
+                              "\"object\":{\"array\":[101,102,103],\"sex\":1,\"sname\":\"XiMenPo\"}"
+                              "}"
+                              ;
+        std::istringstream	is(json);
+        std::ostringstream	os;
+
+        AssertThat(stringify_from_json_stream(data, is),	IsTrue());
+        AssertThat(stringify_to_json_stream(data, os),		IsTrue());
+        AssertThat(os.str(),	Equals(json));
+    }
+
+    Spec(from_stream_array_of_array) {
+        // sorted members.
+        std::string	json	= "{\"age\":34,\"another_data\":\"another_data\","
+                              "\"name\":\"XiMenPo\","
+                              "\"object\":{\"array\":[[101,102,103],[101,102,103],[101,102,103]],\"sex\":1,\"sname\":\"XiMenPo\"}"
+                              "}"
+                              ;
+        std::istringstream	is(json);
+        std::ostringstream	os;
+
+        AssertThat(stringify_from_json_stream(data, is),	IsTrue());
+        AssertThat(stringify_to_json_stream(data, os),		IsTrue());
+        AssertThat(os.str(),	Equals(json));
+    }
+
+    Spec(from_json_empty_object) {
+        std::string	xml	=
+            "{"
+            "}"
+            ;
+
+        std::istringstream	is(xml);
+        std::ostringstream	os;
+        AssertThat(stringify_from_json_stream(data, is),	IsTrue());
+        AssertThat(stringify_to_json_stream(data, os),	IsTrue());
+        AssertThat(os.str(),	Equals(xml));
+    }
+
+    Spec(from_json_empty_array) {
+        std::string	xml	=
+            "["
+            "]"
+            ;
+
+        std::istringstream	is(xml);
+        std::ostringstream	os;
+        AssertThat(stringify_from_json_stream(data, is),	IsTrue());
+        AssertThat(stringify_to_json_stream(data, os),	IsTrue());
+        AssertThat(os.str(),	Equals(xml));
+    }
+
+    Spec(faile_from_json_null) {
+        std::string	xml	=
+            "null"
+            ;
+
+        std::istringstream	is(xml);
+        std::ostringstream	os;
+        AssertThat(stringify_from_json_stream(data, is),	IsFalse());
+    }
+
+    Spec(faile_from_json_simple_value) {
+        std::string	xml	=
+            "34"
+            ;
+
+        std::istringstream	is(xml);
+        std::ostringstream	os;
+        AssertThat(stringify_from_json_stream(data, is),	IsFalse());
+    }
+
+    Spec(from_json_simple_array) {
+        std::string	xml	=
+            "[34,35,36]"
+            ;
+
+        std::istringstream	is(xml);
+        std::ostringstream	os;
+        AssertThat(stringify_from_json_stream(data, is),	IsTrue());
+        AssertThat(stringify_to_json_stream(data, os),	IsTrue());
+        AssertThat(os.str(),	Equals(xml));
+    }
+
+};
