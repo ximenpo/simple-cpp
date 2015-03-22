19cdfebfd61d715fe99aa02f707e9257cfdaac99
ximenpo <ximenpo@jiandan.ren>
2015-03-22 17:46:11 +0800

    对 .hh .cc 文件也进行代码格式化。

--- format_source_code.cmd
+++ format_source_code.cmd
@@ -3,7 +3,7 @@ pushd	%~dp0
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;rem	format source code files.
-for %%E in (h hpp c cpp inc) do (
+for %%E in (h hh hpp c cc cpp inc) do (
 	dir /s /b "inc\*.%%E"	> nul	2<&1	&& tools\AStyle.exe --style=linux --suffix=none --recursive --formatted "inc\*.%%E"	> nul	2<&1
 	dir /s /b "src\*.%%E"	> nul	2<&1	&& tools\AStyle.exe --style=linux --suffix=none --recursive --formatted "src\*.%%E"	> nul	2<&1
 	dir /s /b "tests\*.%%E"	> nul	2<&1	&& tools\AStyle.exe --style=linux --suffix=none --recursive --formatted "tests\*.%%E"	> nul	2<&1
--- inc/simple/third/bigint/BigUnsignedInABase.hh
+++ inc/simple/third/bigint/BigUnsignedInABase.hh
@@ -32,90 +32,98 @@
 class BigUnsignedInABase : protected NumberlikeArray<unsigned short> {
 
 public:
-	// The digits of a BigUnsignedInABase are unsigned shorts.
-	typedef unsigned short Digit;
-	// That's also the type of a base.
-	typedef Digit Base;
+    // The digits of a BigUnsignedInABase are unsigned shorts.
+    typedef unsigned short Digit;
+    // That's also the type of a base.
+    typedef Digit Base;
 
 protected:
-	// The base in which this BigUnsignedInABase is expressed
-	Base base;
+    // The base in which this BigUnsignedInABase is expressed
+    Base base;
 
-	// Creates a BigUnsignedInABase with a capacity; for internal use.
-	BigUnsignedInABase(int, Index c) : NumberlikeArray<Digit>(0, c) {}
+    // Creates a BigUnsignedInABase with a capacity; for internal use.
+    BigUnsignedInABase(int, Index c) : NumberlikeArray<Digit>(0, c) {}
 
-	// Decreases len to eliminate any leading zero digits.
-	void zapLeadingZeros() { 
-		while (len > 0 && blk[len - 1] == 0)
-			len--;
-	}
-
-public:
-	// Constructs zero in base 2.
-	BigUnsignedInABase() : NumberlikeArray<Digit>(), base(2) {}
-
-	// Copy constructor
-	BigUnsignedInABase(const BigUnsignedInABase &x) : NumberlikeArray<Digit>(x), base(x.base) {}
-
-	// Assignment operator
-	void operator =(const BigUnsignedInABase &x) {
-		NumberlikeArray<Digit>::operator =(x);
-		base = x.base;
-	}
-
-	// Constructor that copies from a given array of digits.
-	BigUnsignedInABase(const Digit *d, Index l, Base base);
-
-	// Destructor.  NumberlikeArray does the delete for us.
-	~BigUnsignedInABase() {}
-
-	// LINKS TO BIGUNSIGNED
-	BigUnsignedInABase(const BigUnsigned &x, Base base);
-	operator BigUnsigned() const;
-
-	/* LINKS TO STRINGS
-	 *
-	 * These use the symbols ``0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'' to
-	 * represent digits of 0 through 35.  When parsing strings, lowercase is
-	 * also accepted.
-	 *
-	 * All string representations are big-endian (big-place-value digits
-	 * first).  (Computer scientists have adopted zero-based counting; why
-	 * can't they tolerate little-endian numbers?)
-	 *
-	 * No string representation has a ``base indicator'' like ``0x''.
-	 *
-	 * An exception is made for zero: it is converted to ``0'' and not the
-	 * empty string.
-	 *
-	 * If you want different conventions, write your own routines to go
-	 * between BigUnsignedInABase and strings.  It's not hard.
-	 */
-	operator std::string() const;
-	BigUnsignedInABase(const std::string &s, Base base);
+    // Decreases len to eliminate any leading zero digits.
+    void zapLeadingZeros() {
+        while (len > 0 && blk[len - 1] == 0)
+            len--;
+    }
 
 public:
+    // Constructs zero in base 2.
+    BigUnsignedInABase() : NumberlikeArray<Digit>(), base(2) {}
 
-	// ACCESSORS
-	Base getBase() const { return base; }
+    // Copy constructor
+    BigUnsignedInABase(const BigUnsignedInABase &x) : NumberlikeArray<Digit>(x), base(x.base) {}
 
-	// Expose these from NumberlikeArray directly.
-	using	NumberlikeArray<Digit>::getCapacity;
-	using	NumberlikeArray<Digit>::getLength;
+    // Assignment operator
+    void operator =(const BigUnsignedInABase &x) {
+        NumberlikeArray<Digit>::operator =(x);
+        base = x.base;
+    }
 
-	/* Returns the requested digit, or 0 if it is beyond the length (as if
-	 * the number had 0s infinitely to the left). */
-	Digit getDigit(Index i) const { return i >= len ? 0 : blk[i]; }
+    // Constructor that copies from a given array of digits.
+    BigUnsignedInABase(const Digit *d, Index l, Base base);
 
-	// The number is zero if and only if the canonical length is zero.
-	bool isZero() const { return NumberlikeArray<Digit>::isEmpty(); }
+    // Destructor.  NumberlikeArray does the delete for us.
+    ~BigUnsignedInABase() {}
 
-	/* Equality test.  For the purposes of this test, two BigUnsignedInABase
-	 * values must have the same base to be equal. */ 
-	bool operator ==(const BigUnsignedInABase &x) const {
-		return base == x.base && NumberlikeArray<Digit>::operator ==(x);
-	}
-	bool operator !=(const BigUnsignedInABase &x) const { return !operator ==(x); }
+    // LINKS TO BIGUNSIGNED
+    BigUnsignedInABase(const BigUnsigned &x, Base base);
+    operator BigUnsigned() const;
+
+    /* LINKS TO STRINGS
+     *
+     * These use the symbols ``0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'' to
+     * represent digits of 0 through 35.  When parsing strings, lowercase is
+     * also accepted.
+     *
+     * All string representations are big-endian (big-place-value digits
+     * first).  (Computer scientists have adopted zero-based counting; why
+     * can't they tolerate little-endian numbers?)
+     *
+     * No string representation has a ``base indicator'' like ``0x''.
+     *
+     * An exception is made for zero: it is converted to ``0'' and not the
+     * empty string.
+     *
+     * If you want different conventions, write your own routines to go
+     * between BigUnsignedInABase and strings.  It's not hard.
+     */
+    operator std::string() const;
+    BigUnsignedInABase(const std::string &s, Base base);
+
+public:
+
+    // ACCESSORS
+    Base getBase() const {
+        return base;
+    }
+
+    // Expose these from NumberlikeArray directly.
+    using	NumberlikeArray<Digit>::getCapacity;
+    using	NumberlikeArray<Digit>::getLength;
+
+    /* Returns the requested digit, or 0 if it is beyond the length (as if
+     * the number had 0s infinitely to the left). */
+    Digit getDigit(Index i) const {
+        return i >= len ? 0 : blk[i];
+    }
+
+    // The number is zero if and only if the canonical length is zero.
+    bool isZero() const {
+        return NumberlikeArray<Digit>::isEmpty();
+    }
+
+    /* Equality test.  For the purposes of this test, two BigUnsignedInABase
+     * values must have the same base to be equal. */
+    bool operator ==(const BigUnsignedInABase &x) const {
+        return base == x.base && NumberlikeArray<Digit>::operator ==(x);
+    }
+    bool operator !=(const BigUnsignedInABase &x) const {
+        return !operator ==(x);
+    }
 
 };
 
