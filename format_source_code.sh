4ce2fc13578aef3cc5ecf36369df7ed2fd72090f
ximenpo <ximenpo@jiandan.ren>
2015-05-07 10:36:11 +0800

    对应源代码位置修改, 修改代码格式化脚本.

--- format_source_code.sh
+++ format_source_code.sh
@@ -5,7 +5,7 @@ if [ `uname -s` == 'Darwin' ]; then
     astyle_proc=tools/astyle.Darwin
 fi
 
-for src_dir in inc/simple src/simple tests ; do
+for src_dir in inc/simple tests ; do
 find $src_dir -maxdepth 1 \( -name '*.h' -o -name '*.c' -o -name '*.cpp' -o -name '*.inc' \) -exec $astyle_proc --style=linux --suffix=none --formatted '{}' > /dev/null \;
 find $src_dir -maxdepth 1 \( -name '*.h' -o -name '*.c' -o -name '*.cpp' -o -name '*.inc' \) -exec $astyle_proc --style=java --suffix=none --formatted '{}' > /dev/null \;
 done
