44378d2f060d26b5ffedb6cd3755082d9d45561b
ximenpo <ximenpo@jiandan.ren>
2015-07-04 11:12:13 +0800

    add simple/inttype.s & simple/stdint.h using msinttypes library.

---
+++ inc/simple/inttypes.h
@@ -0,0 +1,10 @@
+#ifndef INTTYPES_H_66BC65DB_AFF6_43C8_8654_D1A2801635E2
+#define INTTYPES_H_66BC65DB_AFF6_43C8_8654_D1A2801635E2
+
+#ifdef _MSC_VER
+#   include "_third/msinttypes/inttypes.h"
+#else
+#   include <inttypes.h>
+#endif
+
+#endif
---
+++ inc/simple/stdint.h
@@ -0,0 +1,10 @@
+#ifndef STDINT_H_66BC65DB_AFF6_43C8_8654_D1A2801635E2
+#define STDINT_H_66BC65DB_AFF6_43C8_8654_D1A2801635E2
+
+#ifdef _MSC_VER
+#   include "_third/msinttypes/stdint.h"
+#else
+#   include <stdint.h>
+#endif
+
+#endif
