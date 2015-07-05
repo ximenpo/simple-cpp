366d2a3dce504be79a3d2d051ea3db368a49ec8a
ximenpo <ximenpo@jiandan.ren>
2015-07-05 10:29:39 +0800

    add xxtea support (btea.h/c).

---
+++ inc/simple/btea.h
@@ -0,0 +1,11 @@
+#ifndef BTEA_H_66BC65DB_AFF6_43C8_8654_D1A2801635E2
+#define BTEA_H_66BC65DB_AFF6_43C8_8654_D1A2801635E2
+
+
+#include "stdint.h"
+
+extern	"C" {
+    void btea(uint32_t *v, int n, uint32_t const key[4]);
+}
+
+#endif
