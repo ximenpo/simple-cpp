d15d392cefa6043045b37b5f39c2b6d4577f5b3f
ximenpo <ximenpo@jiandan.ren>
2016-06-07 13:51:46 +0800

    fix package.cpp compile errors.

--- inc/simple/package.cpp
+++ inc/simple/package.cpp
@@ -1,4 +1,5 @@
 ﻿
+#include	<string.h>
 #include	"package.h"
 
 
@@ -8,13 +9,13 @@ package* package_handler::create_package(int32_t cmd, const void* data, size_t s
     pkg->cmd	= cmd;
     pkg->flag	= flag;
 
-    if(NULL != data && size > 0) {
+    if(0 != data && size > 0) {
         pkg->data	= new char[(size/16 + 1) * 16];
         pkg->size	= size;
 
         memcpy(pkg->data, data, size);
     } else {
-        pkg->data	= NULL;
+        pkg->data	= 0;
         pkg->size	= 0;
     }
 
@@ -22,7 +23,7 @@ package* package_handler::create_package(int32_t cmd, const void* data, size_t s
 }
 
 void package_handler::destroy_package(package* pkg) {
-    if(NULL != pkg) {
+    if(0 != pkg) {
         delete	pkg->data;
         delete	pkg;
     }
